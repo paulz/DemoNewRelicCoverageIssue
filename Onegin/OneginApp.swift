d52f415a5183d063f85d430d4be32aa87756ef6c
Paul Zabelin <paulz@users.noreply.github.com>
2023-05-02 22:21:10 -0700

    Initial Commit

---
+++ Onegin/OneginApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneginApp.swift
+//  Onegin
+//
+//  Created by Artium on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct OneginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
